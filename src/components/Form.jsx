dfc1f6841429e7f17f64f6590879ef35fc9eb762
pataha <pataha@gmx.de>
2025-08-30 18:20:08 +0200

    First commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,17 @@
+const Form = ({getWeather}) => {
+
+    const handleGetWeather = e => {
+        e.preventDefault();
+        const city = e.currentTarget.city.value.trim();
+        getWeather(city);
+    }
+
+    return (
+        <form onSubmit={handleGetWeather}>
+            <input type={"text"} name={"city"}/>
+            <button type={"submit"}>Get Weather</button>
+        </form>
+    );
+};
+
+export default Form;
