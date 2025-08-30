3a78582c2055dd4e12d38a9763139a3728514dcf
pataha <pataha@gmx.de>
2025-08-30 18:49:34 +0200

    Weather old

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,18 +1,18 @@
 const Weather = ({weather, message}) => {
-
-    return (
-        <div className={'infoWeath'}>
-            {!message &&
-                <>
-                    <p>Location: {weather.country}, {weather.city}</p>
-                    <p>Temp: {weather.temp}</p>
-                    <p>Pressure: {weather.pressure}</p>
-                    <p>Sunset: {(new Date(weather.sunset * 1000)).toLocaleTimeString()}</p>
-                </>
-            }
-            {message}
-        </div>
-    )
-}
+    if (message) {
+        return (
+            <div className={'infoWeath'}>{message}</div>
+        )
+    } else {
+        return (
+            <div className={'infoWeath'}>
+                <p>Location: {weather.country}, {weather.city}</p>
+                <p>Temp: {weather.temp}</p>
+                <p>Pressure: {weather.pressure}</p>
+                <p>Sunset: {(new Date(weather.sunset * 1000)).toLocaleTimeString()}</p>
+            </div>
+        );
+    }
+};
 
 export default Weather;
